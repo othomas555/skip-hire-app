ca37bbbe565bbe7f27e854f668de0c87ca2e06eb
othomas555 <o.thomas555@gmail.com>
2025-11-15 00:43:01 +0000

    Create test.js

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+// pages/test.js
+
+export default function TestPage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Test page</h1>
+      <p>If you can see this, routing and rendering are fine.</p>
+    </main>
+  );
+}
