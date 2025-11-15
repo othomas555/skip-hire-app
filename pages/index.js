7ca710120029bc7c8af2987b45be256f0e04db94
othomas555 <o.thomas555@gmail.com>
2025-11-15 00:52:16 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import SkipHireApp from './app';
-
-export default function Home() {
-  return <SkipHireApp />;
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Home page</h1>
+      <p>If you can see this, / is working.</p>
+    </main>
+  );
 }
